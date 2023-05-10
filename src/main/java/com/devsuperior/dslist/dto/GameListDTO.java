fd3920465dc0deaa42cd23cc38d3060bb0907b9e
Barbozha <barbozha@gmail.com>
2023-05-10 11:35:32 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dslist.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameListDTO other = (GameListDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
